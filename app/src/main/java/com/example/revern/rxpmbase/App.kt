cc0f5b0080cf1a4cca95fed686b089e98b75d3c0
Almaz Iskhakov <almaz.iskhakov@flatstack.com>
2018-03-07 01:19:58 +0300

    improve di kodein using

--- app/src/main/java/com/example/revern/rxpmbase/App.kt
+++ app/src/main/java/com/example/revern/rxpmbase/App.kt
@@ -1,24 +1,14 @@
 package com.example.revern.rxpmbase
 
 import android.app.Application
-import com.example.revern.rxpmbase.di.modules.appModule
-import com.example.revern.rxpmbase.di.di
-import com.example.revern.rxpmbase.di.modules.netModule
+import com.example.revern.rxpmbase.di.initKodeinDI
 import com.example.revern.rxpmbase.utils.initializeStetho
-import com.github.salomonbrys.kodein.Kodein
-import com.github.salomonbrys.kodein.KodeinAware
-import com.github.salomonbrys.kodein.lazy
 
-class App : Application(), KodeinAware {
-
-    override val kodein by Kodein.lazy {
-        import(appModule(this@App))
-        import(netModule(BuildConfig.API_BASE_URL))
-    }
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        di = kodein
+        initKodeinDI(this)
         initializeStetho(this)
     }
 
--- app/src/main/java/com/example/revern/rxpmbase/di/DI.kt
+++ app/src/main/java/com/example/revern/rxpmbase/di/DI.kt
@@ -1,5 +1,16 @@
 package com.example.revern.rxpmbase.di
 
+import android.app.Application
+import com.example.revern.rxpmbase.BuildConfig
+import com.example.revern.rxpmbase.di.modules.appModule
+import com.example.revern.rxpmbase.di.modules.netModule
 import com.github.salomonbrys.kodein.Kodein
 
 lateinit var di: Kodein
+
+fun initKodeinDI(app: Application) {
+    di = Kodein {
+        import(appModule(app))
+        import(netModule(BuildConfig.API_BASE_URL))
+    }
+}
